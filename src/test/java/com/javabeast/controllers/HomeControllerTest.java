0fa7f5cb29657312acf5704e481b07e9c339e760
jeffreya <andrew-jeffrey@live.co.uk>
2016-08-14 20:19:23 +0100

    Add mockmvc test for hello world

---
+++ src/test/java/com/javabeast/controllers/HomeControllerTest.java
@@ -0,0 +1,38 @@
+package com.javabeast.controllers;
+
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * Created by jeffreya on 14/08/2016.
+ */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class HomeControllerTest {
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HomeController()).build();
+    }
+
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+    }
+}
