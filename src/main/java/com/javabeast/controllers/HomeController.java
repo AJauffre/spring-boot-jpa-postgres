dab0554e6a2c10345f9543b682822b16a7ab783f
jeffreya <andrew-jeffrey@live.co.uk>
2016-08-15 14:06:17 +0100

    Cleaned up home controller

--- src/main/java/com/javabeast/controllers/HomeController.java
+++ src/main/java/com/javabeast/controllers/HomeController.java
@@ -1,8 +1,5 @@
 package com.javabeast.controllers;
 
-import com.javabeast.dao.CustomerRepository;
-import com.javabeast.entity.Customer;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,19 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class HomeController {
 
-
-    private final CustomerRepository customerRepository;
-
-    @Autowired
-    public HomeController(final CustomerRepository customerRepository) {
-        this.customerRepository = customerRepository;
-    }
-
-
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index() {
-
-        customerRepository.save(Customer.builder().firstName("Andrew").lastName("Jeffrey").build());
         return "Greetings from Spring Boot!";
     }
 }
